dab8977100273b56bd454be9f7cbc8f77037c9d3
Zach Abrahams <zachabrahams@gmail.com>
2017-07-25 23:58:25 -0600

    Adds basic consensus like stuff

--- node.go
+++ node.go
@@ -1,9 +1,17 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
 type Node struct {
 	Address      string
 	Blockchain   []Block
 	Transactions []Transaction
+	NodeURLs     []string
 }
 
 func (n *Node) Mine() error {
@@ -25,6 +33,44 @@ func (n *Node) Mine() error {
 	n.Transactions = []Transaction{}
 	n.Blockchain = append(n.Blockchain, newBlock)
 	return nil
+}
+
+func (n *Node) GetChains() [][]Block {
+	var blockchains [][]Block
+	for _, url := range n.NodeURLs {
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Printf("error fetching blockchain from %s - %+v", url, err)
+			continue
+		}
+		blockchain := []Block{}
+		defer resp.Body.Close()
+		jsonBody, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Printf("error reading response from %s - %+v", url, err)
+			continue
+		}
+		if err := json.Unmarshal(jsonBody, blockchain); err != nil {
+			fmt.Printf("error parsing blockhain json from %s - %+v", url, err)
+			continue
+		}
+
+		blockchains = append(blockchains, blockchain)
+	}
+
+	return blockchains
+}
+
+func (n *Node) Consensus() {
+	longestChain := n.Blockchain
+	blockchains := n.GetChains()
+	for _, blockchain := range blockchains {
+		if len(blockchain) > len(longestChain) {
+			longestChain = blockchain
+		}
+	}
+
+	n.Blockchain = longestChain
 }
 
 func ProofOfWork(lastProof int) int {
